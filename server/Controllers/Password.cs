bc1d3a29a0c040d16385e67827fab5aa4870c1e4
Alain Vezina <mail@alainvezina.com>
2021-07-03 16:30:45 -0400

    Initial

---
+++ server/Controllers/Password.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using server.Models;
+namespace server.Controllers
+{
+    #region TodoController
+    [Route("")]
+    [ApiController]
+    public class PasswordController : ControllerBase
+    {
+
+        public PasswordController()
+        {
+
+        }
+        #endregion
+
+     
+
+ 
+        [Route("")]
+        [Route("{password}")]
+        public async Task<ActionResult<AttemptResult>> Trial(string password)
+        {
+            string currentPassword = DateTime.Now.ToString("yyyyMMdd");
+
+
+            return new AttemptResult(password, password == currentPassword);
+
+        }
+
+    }
+}
---
+++ server/Models/AttemptResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace server.Models
+{
+    public class AttemptResult
+    {
+        public AttemptResult(string password, bool success = false)
+        {
+
+            this.Id = Guid.NewGuid();
+            this.Success = success;
+            this.Password = password;
+            this.AttemptDateTime = DateTime.Now;
+        }
+        public Guid Id { get; set; }
+        public bool Success { get; set; }
+        public string Password { get; set; }
+
+        public DateTime AttemptDateTime { get; set; }
+    }
+}
