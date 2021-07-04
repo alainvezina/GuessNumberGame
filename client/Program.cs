9596c401a65c1177f370b0888efcb199b2f66fe2
Alain Vezina <mail@alainvezina.com>
2021-07-03 23:29:34 -0400

    Add scenario

--- client/Program.cs
+++ client/Program.cs
@@ -22,16 +22,22 @@ namespace client
             if (client == null)
                 GenerateClient();
 
+            await ScenarioFrom1To100();
+            await ScenarioRandomNumberBetween();
+        }
+
+        private static async Task ScenarioFrom1To100()
+        {
             await client.GetStringAsync("reset");
 
             for (int i = 0; i <= 100; i++)
             {
                 var response = await client.GetFromJsonAsync<AttemptResult>(i.ToString());
-                
+
                 if (response.AmIright)
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine($"Attempt #{response.NumberOfAttempts} with number {response.Input} was unsuccesful");
+                    Console.WriteLine($"Attempt #{response.NumberOfAttempts} with number {response.Input} was sucessfull");
                     Console.ResetColor();
                     break;
                 }
@@ -41,6 +47,50 @@ namespace client
                 }
             }
             Console.ReadKey();
+            Console.Clear();
+        }
+
+        private static async Task ScenarioRandomNumberBetween()
+        {
+            await client.GetStringAsync("reset");
+            var rand = new Random();
+            int numberOfAttempts = 0;
+            int input = 50;
+            int lowNumber = 0;
+            int maxNumber = 100;
+            
+            do
+            {
+                var response = await client.GetFromJsonAsync<AttemptResult>(input.ToString());
+
+                if (response.AmIright)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Attempt #{response.NumberOfAttempts} with number {response.Input} was sucessfull");
+                    Console.ResetColor();
+                    break;
+                }
+                else
+                {
+                    if (response.IsLowerOrEqual)
+                    {
+                        maxNumber = input;
+                        input = rand.Next(lowNumber, input);
+                    }
+                    else
+                    {
+                        lowNumber = input + 1;
+                        input = rand.Next(lowNumber, maxNumber);
+                    }    
+
+                    Console.WriteLine($"Attempt #{response.NumberOfAttempts} with number {response.Input} was unsuccesful.");
+                }
+
+                numberOfAttempts++;
+            } while (numberOfAttempts <= 100);
+
+            Console.ReadKey();
+            Console.Clear();
         }
     }
 }
